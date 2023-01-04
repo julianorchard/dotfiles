c56d38216404988c1f945ddcc3b340d02e2797da
Julian Orchard <hello@julianorchard.co.uk>
2023-01-04 09:56:49 +0000

    feat: finalising the nitter-rss-generator script

--- .bin/nitter-rss-generator.py
+++ .bin/nitter-rss-generator.py
@@ -16,69 +16,103 @@
 
 import tweepy
 
-## get from ~/Documents/Code/crypt file
+# Set the rate limit of how many accounts to check
+RATE_LIMIT = 20
 
-keys_file_path = "C:/Users/jorchard/Documents/Code/crypt/twitter_6a756c69616e"
+# Nitter URL you want to use
+NITTER_URL = "https://nitter.net/"
 
-def spl_line(line):
+# Key file path. Looks for the line containing "BEARER_TOKEN" 
+# then include the bearer token there!
+KEY_FILE_PATH = "C:/Users/jorchard/Documents/Code/crypt/twitter_6a756c69616e"
+
+# Making this global, but we only need it for the auth
+BEARER = ""
+
+
+def get_key_file():
     '''
-    Splits the damn line of the keys file.
+    This gets the key from a file. It looks for the line containing
+    "BEARER_TOKEN" and then gets the bearer token next to it. 
     '''
-    return line.split(":")[1]
-   
-
-with open(keys_file_path) as file:
-    for line in file:
-        if "API_KEY" in line:
-            API_KEY = spl_line(line)
-        elif "API_SECRET" in line:
-            API_SECRET = spl_line(line)
-        elif "ACCESS_TOKEN" in line:
-            ACCESS_TOKEN = spl_line(line)
-        elif "ACCESS_SECRET" in line:
-            ACCESS_SECRET = spl_line(line)
-        elif "BEARER_TOKEN" in line:
-            BEARER = spl_line(line)
-        else:
-            print("Error, key problem.")
+    try: 
+        with open(KEY_FILE_PATH) as file:
+            for line in file:
+                if "BEARER_TOKEN" in line:
+                    return line.split(":")[1].replace("\n", "").replace(" ", "")
+            # No line with BEARER_TOKEN found
+            print(f"No BEARER_TOKEN line found in {KEY_FILE_PATH} file.")
+    except FileNotFoundError: 
+        print(f"Error opening file {KEY_FILE_PATH}, please try again.")
 
 def client_auth():
-    '''
-    Return client authentification.
-    '''
+    '''Return client authentication using the bearer token.'''
     return tweepy.Client(bearer_token=BEARER)
 
 def following_list_nitter_rss(client, user_id):
-    '''
-    Get the list of the user's current following!
-    '''
-    r = client.get_users_following(
-        user_id
+    '''Get the input user's current following list.'''
+    results = client.get_users_following(
+        user_id,
+        max_results=RATE_LIMIT
     )
-    c = 1
-    o = "https://nitter.eu/"
-    for u in r.data:
-        if c > 20:
-            return o
-        else:
-            c = c + 1
-        o = o + f"{u.username},"
-    # return o
+    following_list = []
+    for user in results.data:
+        following_list.append(f"{user.username}")
+    return following_list
 
 def get_id_from_username(client, usrnm):
-    '''
-    Returns the ID from a given username (handle/@).
-    '''
+    '''Returns the ID from a given username (handle or '@').'''
     return client.get_user(username=usrnm).data.id
 
+def output_data_nicely(full_following_list):
+    '''
+    Split output into strings with the format: 
+    https://nitter.net/user1,user2,(...),user20
+
+    It splits at 20, basically. 
+    
+    I think this is a limitation of nitter; that's 
+    the number I'm using, anyway.
+    '''
+    total_count = 0 # Just used in user feedback
+    split_count = 0 # Used to count the groups of 20
+    split_list = [] # Contains the groups of 20
+
+    for full_list_item in full_following_list:
+
+        total_count = total_count + 1
+        split_count = split_count + 1
+
+        # Comma seperating the list
+        split_list.append(f"{full_list_item},")
+
+        if split_count >= 20: 
+            output_url = NITTER_URL
+            for list_item in split_list:
+                output_url = f"{output_url}{list_item}"
+
+            # Remove last character (a trailing comma)
+            output_url = output_url[:-1]
+
+            print(f"{output_url}")
+
+            # Reset counters and empty split_list array
+            # for next group of 20
+            split_list = []
+            split_count = 0
+    print(f"There were following {total_count} accounts, listed in the lists above!")
+
 def main():
+    # This isn't a good method...
+    BEARER = get_key_file()
 
     client = client_auth()
     
     usrnm = input("Enter a username: ")
     usr = get_id_from_username(client, usrnm)
-
-    print(following_list_nitter_rss(client, usr))
+    
+    l = following_list_nitter_rss(client, usr)
+    output_data_nicely(l)
 
 if __name__ == "__main__":
     main()
