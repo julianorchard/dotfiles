eb697df53dc8ed71006c91ce0bec69743ecb0107
Julian Orchard <hello@julianorchard.co.uk>
2023-01-05 10:48:46 +0000

    feat: working git-commit-date-fix.sh script

--- .bin/git-commit-date-fix.sh
+++ .bin/git-commit-date-fix.sh
@@ -21,8 +21,7 @@
 # we should use that as the default results
 [ "${1}" ] && DEFAULT_RESULTS=$1 || DEFAULT_RESULTS=5
 
-# TODO: Find a better place for this (/tmp?)
-LOG_OUTPUT="temp-log-output.txt"
+LOG_OUTPUT="$TMPDIR/git-commit-date-fix-temp-output.txt"
 COMMIT_LINE="commit "
 
 function main() {
@@ -37,56 +36,70 @@ function main() {
   # Get the current `git log` to output
   git log>"${LOG_OUTPUT}"
 
-  # TODO:
-  # Thinking about it, we could achieve the same thing with a counter
-  # where we just increment it by X from the line we match. Then we
-  # just reset and stop incrememting it past a certain point.
+  match_counter=0
 
-  total_counter=0
-  c=0
+  # pass_* are responsible for getting the line with the commit message
+  pass_counter=0
+  pass_boolean=0
   commit_id_array=()
 
-  while read p; do
-    # ${total_counter} will be used to calculate the position of the
-    # commit message, which the user will use to ID the commit to select
-    total_counter=$((total_counter + 1))
-
+  while read line; do
     # Check line contains 'commit '
-    if echo "$p" | grep -q "${COMMIT_LINE}"; then
-      # ${c} is the index of the array to select from later
-      c=$((c+1))
-      # ${p} is the only value needed in the array we're building
-      commit_id_array+=(${p:7})
-      # Output the line containing the commit message. Calculated by using
-      # ${total_counter} (current line) + 5 (how far away it is!)
-      commit_message_line=$((total_counter + 4))
-      commit_message=$(sed "${commit_message_line}q;d" ${LOG_OUTPUT})
-      echo "${c}) ${commit_message} (${p:7})"
+    if echo "$line" | grep -q "${COMMIT_LINE}"; then
+
+      # ${match_counter} is the index of the array to select from later
+      match_counter=$((match_counter+1))
+
+      # ${line} is the only value needed in the array being built:
+      # becomes ${commit_id_array} (after cutting "commit " substr), here
+      commit_id_array+=(${line:7})
+
+      # Alternative method means we don't need to keep seding the file
+      pass_boolean=1
+    fi
+
+    # Initially, I used sed to get the line with the commit message, but it
+    # was extremely slow. This just sets a bool and a counter in motion, where,
+    # when we get 4 lines past the "commit HASH" match line (above), we know
+    # we're at the line with the commit message:
+    if [ $pass_boolean -eq 1 ] && [ $pass_counter -lt 4 ] ; then
+      # Increment pass_counter if we're matched "commit HASH" but not
+      # yet reached the correct line (4 down)
+      pass_counter=$((pass_counter + 1))
+    elif [ $pass_boolean -eq 1 ] && [ $pass_counter -ge 4 ] ; then
+      # Output the line which the user can use to identify the right
+      # commit to select. Then reset the counter/bool
+      echo "$match_counter) $line"
+      pass_counter=0
+      pass_boolean=0
     fi
 
     # Stop listing results after the DEFAULT RESULTS amount
-    [ ${c} -eq ${DEFAULT_RESULTS} ] && break
+    [ ${match_counter} -eq ${DEFAULT_RESULTS} ] && break
   done<"${LOG_OUTPUT}"
 
   # We don't need the log output file anymore
   rm $LOG_OUTPUT
 
-  # get the line containing "commit " in `git log` output, then 4 lines down
+  while : ; do
+    echo "Enter a number from above: "
+    read input_number
 
-  echo "Enter a number from above: "
-  read input_number
-  # minus one because array
-  input_number=$((input_number - 1))
+    # Only break if the input_number is in the range allowed (for
+    # which the best value to use is match_counter):
+    [ ! $input_number -lt 1 ] && [ ! $input_number -gt $match_counter ] && break
+  done
 
+  # TODO: Would be nicer to make this easier to do
   echo "Enter date (format; Mon Jan 1 00:00:00 2020 +0000): "
   read date_string
 
-  # hol' onto ur butts
-  git filter-branch --env-filter \
-      "if [ $GIT_COMMIT = ${commit_id_array[${input_number}]} ] ; then
-          export GIT_AUTHOR_DATE=\"${date_string}\"
-          export GIT_COMMITTER_DATE=\"${date_string}\"
-      fi"
+  # Make the git change
+  git filter-branch -f --env-filter \
+    "if [ \$GIT_COMMIT = ${commit_id_array[$((${input_number} - 1))]} ] ; then
+       export GIT_AUTHOR_DATE=\"${date_string}\"
+       export GIT_COMMITTER_DATE=\"${date_string}\"
+     fi"
 
 }
 
