53fa6f227362c11e609a2728378a85a56df08d0f
Julian Orchard <hello@julianorchard.co.uk>
2023-01-30 14:19:09 +0000

    fix: moving files around and fixing init-aesthetics.el in new location

--- .emacs.d/lisp/init-aesthetics.el
+++ .emacs.d/lisp/init-aesthetics.el
@@ -5,53 +5,21 @@
 ;; Author:       Julian Orchard <jorchard@pm.me>
 ;; Keywords:     lisp, functions
 ;; Date Created: 2023-01-25
-;; Date Updated: 2023-01-25
+;; Date Updated: 2023-01-30
 
 ;;; Description:
 
 ;; Contains everything to do with how Emacs looks. I use the modus-themes from
-;; the amazing Protesilaos, Doom Emacs modeline, Fira as my font of choice,
-;; and, of course, Nyan mode. 
+;; the amazing Protesilaos, Doom Emacs modeline, and, of course, Nyan mode.
+
+;; The only bits omitted are the 'remove top menu' and things; they're in
+;; `init.el`, because it's nice to have them removed ASAP after loading Emacs.
 
 ;;; License:
 
 ;; See /LICENSE file in the root of this repository.
 
 ;;; Code:
-
-
-;; General Guff Removal
-
-(scroll-bar-mode -1)
-(tool-bar-mode -1)
-(tooltip-mode -1)
-(set-fringe-mode 5)
-(menu-bar-mode -1)
-(show-paren-mode t)
-(setq visible-bell t
-      split-width-threshold 1)
-
-
-;; Lines and Columns
-
-(column-number-mode)
-(setq display-line-numbers-type 'relative)
-(dolist (mode '(text-mode-hook
-                prog-mode-hook
-                conf-mode-hook))
-  (add-hook mode (lambda ()
-		   (display-line-numbers-mode 1))))
-
-(dolist (rm-ln-hook '(org-mode-hook))
-  (add-hook rm-ln-hook (lambda () (display-line-numbers-mode 0))))
-
-
-;; Default and Italic
-
-(defvar global-text-height 90)
-(set-face-attribute 'default nil :font "Fira Code Retina" :height global-text-height)
-(set-face-attribute 'italic nil :font "Fira Mono" :height global-text-height)
-
 
 ;; Emoji
 
@@ -83,6 +51,9 @@ dark theme for our vision."
               (number-sequence 7 20))
       (set-light-theme)
     (set-dark-theme)))
+
+(timed-theme)
+;; (run-at-time '15 minutes' (timed-theme))
 
 
 ;; Modeline
