c432e04893c9eebf88e08b27406e18c90dc1e4ce
Julian <hello@julianorchard.co.uk>
2024-02-05 00:53:16 +0000

    Delete .ahk/ahk-startup.ps1

--- .ahk/ahk-startup.ps1
+++
@@ -1,5 +0,0 @@
-$userEnv     = $ENV:UserProfile
-$appData     = "\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
-$startupPath = "$userEnv$appData"
-
-Set-Shortcut -LinkPath "$startupPath\general.ahk.lnk" -TargetPath "$userEnv\.ahk\general.ahk"
