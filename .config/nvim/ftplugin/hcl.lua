b8be7816cf89a7a92f144081258959bf0000e01c
Julian Orchard <hello@julianorchard.co.uk>
2024-05-09 18:24:57 +0100

    feat: AwesomeWM instead of i3

--- .config/nvim/ftplugin/hcl.lua
+++ .config/nvim/ftplugin/hcl.lua
@@ -1,12 +1,12 @@
--- Run terragrunt format on save
-vim.api.nvim_create_autocmd(
-  "BufWritePost", {
-    command = [[ :silent! !terragrunt hclfmt --terragrunt-hclfmt-file % ]]
-  }
-)
-
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 
 vim.opt.commentstring = "#%s"
+
+-- Run terragrunt format on save
+-- vim.api.nvim_create_autocmd(
+--   "BufWritePost", {
+--     command = [[ :silent! !terragrunt hclfmt --terragrunt-hclfmt-file % ]]
+--   }
+-- )
